42c8c571761d814f8d1132f4676c70bb74007975
kartikey <p.kartikey.p@gmail.com>
2025-12-15 23:23:16 +0530

    transcriptionapi added

--- app/src/main/java/com/example/rec/data/network/TranscriptionApi.kt
+++ app/src/main/java/com/example/rec/data/network/TranscriptionApi.kt
@@ -5,7 +5,6 @@ import retrofit2.Response
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
-
 interface TranscriptionApi {
     @Multipart
     @POST("transcribe")
